b0cf9de77cf66907ef771732f79ebd3b9d3e19cf
Ramon Asuncion <asuncionbatista@gmail.com>
2025-11-08 01:43:47 -0500

    Update shebang

--- clean.sh
+++ clean.sh
@@ -1,4 +1,4 @@
-#!/user/bin/env bash
+#!/usr/bin/env bash
 # This is to create new models. I have my code all of the places. Just an easy way to do this for now
 rm -rf dataset
 rm -rf third_party/TextyMcSpeechy/tts_dojo/DATASETS/custom_voice
